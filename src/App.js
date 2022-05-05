affb61ce6f85b1a64be3543c8f158df0e092d799
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-05-05 17:06:24 +0100

    fix route issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 import { useEffect, useState } from "react";
